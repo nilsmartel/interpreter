19984a6ed2923062da2bf81775a14551b05e2121
Nils Martel <nilsmartel@yahoo.de>
2022-08-23 12:20:00 +0200

    Start parsing

--- parsing/ast.go
+++ parsing/ast.go
@@ -1,7 +1,30 @@
 package parsing
 
-import "interpreter/ast"
+import (
+	"interpreter/ast"
+)
 
 func Parse(tokens []Token) ([]ast.Expression, error) {
 
+}
+
+// (a b c)
+func ParseList(tokens []Token) (ast.Expression, error) {
+	if tokens[0].Tag != ParenOpen {
+		return nil, Expected{Candidates: "("}
+	}
+
+	// TODO parse list of expressions
+}
+
+type Expected struct {
+	Candidates string
+	or         *Expected
+}
+
+func (e Expected) Error() string {
+	if e.or != nil {
+		return e.Candidates + " " + e.or.Error()
+	}
+	return e.Candidates
 }
