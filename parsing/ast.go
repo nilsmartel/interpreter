ab6f3595003de94dfe7871873ee9372e61a520b5
Nils Martel <nilsmartel@yahoo.de>
2022-08-23 12:44:20 +0200

    Implement more parsing operations

--- parsing/ast.go
+++ parsing/ast.go
@@ -2,19 +2,89 @@ package parsing
 
 import (
 	"interpreter/ast"
+	"strconv"
+	"errors"
 )
 
-func Parse(tokens []Token) ([]ast.Expression, error) {
+func Parse(tokens []Token) (ast.Expression, []Token, error) {
+	if len(tokens) == 0 {
+		return nil, tokens, Expected{Candidates: "<expr>"}
+	}
 
+	fst := tokens[0]
+	rest := tokens[1:]
+	switch fst.Tag {
+	case Identifier:
+		return ast.IdentLiteral{Value: fst.Span}, rest, nil
+	case Int:
+		value, err := strconv.ParseInt(fst.Span, 0, 64)
+		if err != nil {
+			return nil, nil, err
+		}
+		return ast.IntLiteral{Value: value}, rest, nil
+	case Float:
+		value, err := strconv.ParseFloat(fst.Span,  64)
+		if err != nil {
+			return nil, nil, err
+		}
+		return ast.FloatLiteral{Value: value}, rest, nil
+	case String:
+		value := parseString(fst.Span)
+		return ast.StringLiteral{Value: value}, rest, nil
+
+	case ParenOpen:
+		return parseList(rest)
+
+	case BracketOpen:
+		return parseArray(rest)
+
+	case EndOfInput:
+		return nil, nil, errors.New("unexpected end of input")
+	}
+
+	return nil, nil, errors.New("should be unreachable")
 }
 
 // (a b c)
-func ParseList(tokens []Token) (ast.Expression, error) {
-	if tokens[0].Tag != ParenOpen {
-		return nil, Expected{Candidates: "("}
+func parseList(tokens []Token) (ast.Expression, []Token, error) {
+	// while not Paren Close: Parse
+}
+
+func parseString(raw string) string {
+	// trim " " chars.
+	raw = raw[1:]
+	raw = raw[:len(raw)-1]
+
+	buffer := ""
+
+	escaped := true
+	for _,c := range raw {
+		if escaped {
+			switch c {
+			case 'n':
+				buffer += "\n"
+			case 't':
+				buffer += "\t"
+			case '\\':
+				buffer += "\\"
+			case 'r':
+				buffer += "\r"
+			default:
+				buffer += string(c)
+			}
+			escaped = false
+			continue
+		}
+
+		if c == '\\' {
+			escaped = true
+			continue
+		}
+
+		buffer += string(c)
 	}
 
-	// TODO parse list of expressions
+	return buffer
 }
 
 type Expected struct {
@@ -28,3 +98,4 @@ func (e Expected) Error() string {
 	}
 	return e.Candidates
 }
+
