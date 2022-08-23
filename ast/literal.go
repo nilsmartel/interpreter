833c32ecadad936b9077679ad6c359909670719d
Nils Martel <nilsmartel@yahoo.de>
2022-08-23 09:59:22 +0200

    Include new literals

--- ast/literal.go
+++ ast/literal.go
@@ -4,6 +4,8 @@ type IdentLiteral struct {
 	Value string
 }
 
+type NilLiteral struct{}
+
 type BoolLiteral struct {
 	Value bool
 }
@@ -19,6 +21,10 @@ type FloatLiteral struct {
 // TODO might want to do this more complex, include string interpolation
 type StringLiteral struct {
 	Value string
+}
+
+type ArrayLiteral struct {
+	Values []Expression
 }
 
 // (fun [x y z] (+ x (+ y z)))
