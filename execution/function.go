58aeb578f417b4c5c8de8b4aa27338c0a73b1ed2
Nils Martel <nilsmartel@yahoo.de>
2022-09-01 12:28:51 +0200

    Document ideas

--- execution/function.go
+++ execution/function.go
@@ -11,6 +11,27 @@ type PatternMatch struct {
 	function Callable
 }
 
+// types of patterns:
+
+// exactly 3 arguments
+// (x y z)
+
+// anything goes (e.g. print function)
+// (& x)
+
+// at least 2 arguments
+// (x y & rest)
+
+// 2 arguments
+// arg1.type == Array
+// (x:Array f)
+
+//
+// ("hello")
+// (0) (...)
+// (1) (...)
+// (n) (...)
+
 func (f *Function) Str() string {
 	return "(...)"
 }
