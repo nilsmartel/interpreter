a13bfa5efbfed24dd7061ccffa9b7308702b7e1f
Nils Martel <nilsmartel@yahoo.de>
2022-10-20 12:16:52 +0200

    Include new sample

--- sample/class-methods.lisp
+++ sample/class-methods.lisp
@@ -1,6 +1,16 @@
 (class Point (x y)
-       fun +(self other)
-            (Point (+ (x self) (x other)) (+ (y self) (x self))))
+       (fun + [self other]
+            (Point (+ (x self) (x other)) (+ (y self) (x self)))))
+
+(class BufferedWriter
+       (_vec w)
+       (fun write [self data]
+            (do
+                (push (_vec self) data)
+                (if (> (len _vec) 1024) (do
+                                          (write (w self) (_vec self))
+                                          (assign (_vec self) [])
+                                          true) false))
 
 (print (let a (Point 1 2)
 (let b (Point 2 3)
